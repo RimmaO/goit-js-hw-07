565324da1062bbaea44346c142304579e931afa4
RimmaO <rimma_o@ukr.net>
2023-04-28 20:58:21 +0300

    updated

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -50,7 +50,7 @@ function onImageClick(event) {
   galleryList.addEventListener("keydown", (event) => {
     if (event.code === "Escape") {
       instance.close();
+      galleryList.removeEventListener("click", onImageClick);
     }
-    galleryList.removeEventListener("click", onImageClick);
   });
 }
